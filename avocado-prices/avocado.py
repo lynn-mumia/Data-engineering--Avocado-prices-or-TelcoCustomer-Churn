8b9cc4edc081cbf587b8127e4f523035ab47b99b
Lynn Mumia <lynn.mumia@ashesi.edu.gh>
2018-10-30 08:26:21 +0000

    Additional exploratory data analysis and plots

--- avocado-prices/avocado.py
+++ avocado-prices/avocado.py
@@ -1,4 +1,4 @@
-#This is the code for the avocado prices prediction code
+#This is the Avocado Prices prediction code
 #@author Lynn Mumia
 
 
@@ -6,7 +6,8 @@
 import csv
 import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
+#import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 #Reading the data into a dataframe
@@ -22,7 +23,37 @@ avocado.tail(5)
 
 #Checking for important statistics details for the numeric variables
 avocado.describe()
+#Average price= $1.4
+#Maximum price= $3.25
+#Minimum price= $0.44
 
 
-#Checking null values
+#Labelling categorical columns in the data
+avocado= avocado.astype(dtype={
+    'type':'category','year':'category','region':'category'})
+
+#Converting the date column to a datatime
+avocado['Date']= pd.to_datetime(avocado['Date'], format='%Y-%m-%d')
+
+#Sorting the data by date
+avocado=avocado.sort_values(by='Date', ascending=True)
+
+
+#Checking for null values
 avocado.isnull().values.any()
+#No null values found in the dataset
+
+#Understanding categorical columns
+categoricals_col = avocado.select_dtypes(include=['category']).copy()
+categoricals_col.describe()
+
+avocado.info()
+
+
+#Plots to explore the volumes of avocadoes with respect to date
+x=avocado['Date']
+y=avocado['Total Volume']
+plt.plot(x,y)
+plt.show()
+
+
